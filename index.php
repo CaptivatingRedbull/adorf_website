725ea47e4cc22bf5cf6bfec1364d5d3bcf196501
root <root@alice.hof-university.de>
2025-03-12 14:14:26 +0100

    Impressum Link

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
 <!-- Footer -->
 <footer>
     <div class="container">
-        <p>&copy; 2025 Stadt Adorf. Alle Rechte vorbehalten.| <a href="impressum.html">Impressum</a></p>
+        <p>&copy; 2025 Stadt Adorf. Alle Rechte vorbehalten. | <a href="impressum.html">Impressum</a></p>
     </div>
 </footer>
 </body>
