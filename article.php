b3cea26b8499791b3455041bc6f4b210547edca7
BinInaktiv <janik.peisker@gmail.com>
2025-03-12 21:14:10 +0100

    test

--- article.php
+++ article.php
@@ -24,26 +24,68 @@
     </div>
 </header>
 
-<main class="article-container">
-    <article class="full-article">
-        <h1>Bert Meier von seinem Hund Bello gebissen</h1>
-        <p class="article-meta">Veröffentlicht am 12. März 2025 von <strong>Redaktion Adorf</strong></p>
+<!--    <main class="article-container">
+        <article class="full-article">
+            <h1>Bert Meier von seinem Hund Bello gebissen</h1>
+            <p class="article-meta">Veröffentlicht am 12. März 2025 von <strong>Redaktion Adorf</strong></p>
 
-        <img src="images/hund.jpg" alt="Hund Bello" class="article-image">
+            <img src="images/hund.jpg" alt="Hund Bello" class="article-image">
 
-        <p>Gestern Nachmittag kam es zu einem Vorfall in der Musterstraße, bei dem <strong>Bert Meier</strong> von seinem eigenen Hund <em>Bello</em> gebissen wurde. Der 53-Jährige wurde umgehend ins Krankenhaus eingeliefert.</p>
+            <p>Gestern Nachmittag kam es zu einem Vorfall in der Musterstraße, bei dem <strong>Bert Meier</strong> von seinem eigenen Hund <em>Bello</em> gebissen wurde. Der 53-Jährige wurde umgehend ins Krankenhaus eingeliefert.</p>
 
-        <p>Der Vorfall ereignete sich gegen 14:30 Uhr. Nachbarn berichten, dass Bello plötzlich aggressiv wurde. Laut Aussagen des Tierarztes könnte ein medizinisches Problem Grund für das Verhalten des Tieres gewesen sein.</p>
+            <p>Der Vorfall ereignete sich gegen 14:30 Uhr. Nachbarn berichten, dass Bello plötzlich aggressiv wurde. Laut Aussagen des Tierarztes könnte ein medizinisches Problem Grund für das Verhalten des Tieres gewesen sein.</p>
 
-        <p>Die Verletzungen von Herrn Meier sind glücklicherweise nicht lebensbedrohlich. Er konnte das Krankenhaus noch am selben Abend wieder verlassen. Der Hund wird derzeit von Fachleuten untersucht.</p>
+            <p>Die Verletzungen von Herrn Meier sind glücklicherweise nicht lebensbedrohlich. Er konnte das Krankenhaus noch am selben Abend wieder verlassen. Der Hund wird derzeit von Fachleuten untersucht.</p>
 
-        <blockquote>„Ich liebe Bello – aber ich muss verstehen, was ihn so erschreckt hat“, sagte Bert Meier nach dem Vorfall.</blockquote>
+            <blockquote>„Ich liebe Bello – aber ich muss verstehen, was ihn so erschreckt hat“, sagte Bert Meier nach dem Vorfall.</blockquote>
 
-        <p>Die Stadt Adorf weist darauf hin, dass Halter stets ein wachsames Auge auf das Verhalten ihrer Tiere haben sollten. Weitere Informationen zur Hundehaltung gibt es im Bereich <a href="index.html#services">Bürgerdienste</a>.</p>
+            <p>Die Stadt Adorf weist darauf hin, dass Halter stets ein wachsames Auge auf das Verhalten ihrer Tiere haben sollten. Weitere Informationen zur Hundehaltung gibt es im Bereich <a href="index.html#services">Bürgerdienste</a>.</p>
 
-        <a href="index.php#news" class="btn">Zurück zu den Nachrichten</a>
-    </article>
-</main>
+            <a href="index.php#news" class="btn">Zurück zu den Nachrichten</a>
+        </article>
+    </main> -->
+    <section id="impressum">
+        <h2>Impressum</h2>
+
+        <div class="impressum-block">
+            <h3>Angaben gemäß § 5 TMG</h3>
+            <p>
+                Stadt Adorf<br>
+                Musterstraße 1<br>
+                91522 Adorf<br>
+                Deutschland
+            </p>
+        </div>
+
+        <div class="impressum-block">
+            <h3>Vertreten durch</h3>
+            <p>Oberbürgermeisterin Erika Beispiel</p>
+        </div>
+
+        <div class="impressum-block">
+            <h3>Kontakt</h3>
+            <p>
+                Telefon: 0981 123456<br>
+                Telefax: 0981 654321<br>
+                E-Mail: <a href="mailto:info@stadt-adorf.de">info@stadt-adorf.de</a>
+            </p>
+        </div>
+
+        <div class="impressum-block">
+            <h3>Umsatzsteuer-ID</h3>
+            <p>Umsatzsteuer-Identifikationsnummer gemäß § 27a Umsatzsteuergesetz:<br>
+            DE987654321</p>
+        </div>
+
+
+        <div class="impressum-block">
+            <h3>Haftungsausschluss</h3>
+            <p>
+                Trotz definitiv sehr, sehr sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links.<br>
+                Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
+            </p>
+        </div>
+</section>
 <footer>
     <div class="container">
         <p>&copy; 2025 Stadt Adorf. Alle Rechte vorbehalten. | <a href="impressum.php">Impressum</a></p>
