601abd04dc4152e237e60159614771a39f061a45
CaptivatingRedbull <Marlon.schoeffend@icloud.com>
2025-03-13 14:06:07 +0100

    bla

--- change_password.php
+++ change_password.php
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Passwort ändern</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 <div class="form-container">
