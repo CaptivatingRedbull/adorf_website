5e66f48354633d8a2ffa04c6dee7e02876ab46c4
BinInaktiv <janik.peisker@gmail.com>
2025-03-13 10:10:05 +0100

    nochmal

--- index.php
+++ index.php
@@ -33,6 +33,11 @@
         <a href="#services" class="btn">Mehr erfahren</a>
         <a href="login.php" class="btn">Login</a>
     </div>
+    <div class="hero-wave">
+    <svg viewBox="0 0 1440 100">
+      <path fill="#fff" d="M0,0 C480,100 960,0 1440,100 L1440,0 L0,0 Z"></path>
+    </svg>
+  </div>
 </section>
 
 <!-- Leistungs-Bereich -->
